6ec1ba700bcdadeed194003db9afc68b6b7ed8ec
sunweiguang <sunweiguang@davdian.com>
2018-08-31 14:05:26 +0800

    [其他]updated [影响]无

---
+++ app/src/main/java/com/swg/news_android/MainActivity.java
@@ -0,0 +1,174 @@
+package com.swg.news_android;
+
+import android.app.Activity;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.View;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.ViewGroup;
+import android.graphics.Color;
+import android.graphics.Point;
+//import android.support.v4.widget.DrawerLayout;
+//import android.graphics.Color;
+import android.webkit.WebChromeClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setStatusBarFullTransparent();
+        setFitSystemWindow(false);
+
+        init();
+    }
+
+    private void init() {
+        webView = (WebView) findViewById(R.id.webView);
+
+        // 需要加载的网页的url
+//        webView.loadUrl("https://18686604386.davdian.com/");
+//        webView.loadUrl("http://localhost:7001/");
+//        webView.loadUrl("http://127.0.0.1:7001/");
+        webView.loadUrl("http://192.168.1.111:7001/");
+        WebSettings settings = webView.getSettings();
+
+        // 如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        settings.setUserAgentString(webView.getSettings().getUserAgentString() + " StatusBarHeight/" + getStatusBarHeightPx());
+
+        // 解决alert不出现的问题
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //等待证书响应
+                handler.proceed();
+            }
+        });
+    }
+
+
+    /**
+     * 全透状态栏
+     */
+    protected void setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    protected void setHalfTransparent() {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局fitsSystemWindows=true。
+     */
+    private View contentViewGroup;
+
+    protected void setFitSystemWindow(boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
+    /**
+     * 为了兼容4.4的抽屉布局->透明状态栏
+     */
+//    protected void setDrawerLayoutFitSystemWindow() {
+//        if (Build.VERSION.SDK_INT == 19) {//19表示4.4
+//            int statusBarHeight = getStatusHeight(this);
+//            if (contentViewGroup == null) {
+//                contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+//            }
+//            if (contentViewGroup instanceof DrawerLayout) {
+//                DrawerLayout drawerLayout = (DrawerLayout) contentViewGroup;
+//                drawerLayout.setClipToPadding(true);
+//                drawerLayout.setFitsSystemWindows(false);
+//                for (int i = 0; i < drawerLayout.getChildCount(); i++) {
+//                    View child = drawerLayout.getChildAt(i);
+//                    child.setFitsSystemWindows(false);
+//                    child.setPadding(0,statusBarHeight, 0, 0);
+//                }
+//
+//            }
+//        }
+//    }
+
+
+    /**
+     * 获取状态栏高度
+     */
+//    int getStatusBarHeight() {
+//        WindowManager wm = this.getWindowManager();
+//
+//        Point point=new Point();
+//        wm.getDefaultDisplay().getSize(point);
+//        int width = point.x;
+//        int height = point.y;
+//
+//        int result = 0;
+//        int resourceId = this.context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+//        if (resourceId > 0) {
+//            result = this.context.getResources().getDimensionPixelSize(resourceId);
+//        }
+//
+//        return result;
+//    }
+    private float getStatusBarHeightPx() {
+        int result = 0;
+        int resourceId = this.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = this.getResources().getDimensionPixelSize(resourceId);
+        }
+
+        float density = this.getResources().getDisplayMetrics().density;
+
+        float px = result / density;
+
+        return px;
+    }
+}
