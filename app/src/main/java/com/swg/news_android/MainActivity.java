0cdc3276ff1c06e5f251c36da905060dc672f771
sunweiguang <sunweiguang@davdian.com>
2018-09-02 01:32:40 +0800

    [其他]updated [影响]无

--- app/src/main/java/com/swg/news_android/MainActivity.java
+++ app/src/main/java/com/swg/news_android/MainActivity.java
@@ -17,6 +17,7 @@ import android.graphics.Point;
 //import android.support.v4.widget.DrawerLayout;
 //import android.graphics.Color;
 import android.webkit.WebChromeClient;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
     private WebView webView;
@@ -52,6 +53,7 @@ public class MainActivity extends Activity {
         });
 
         settings.setUserAgentString(webView.getSettings().getUserAgentString() + " StatusBarHeight/" + getStatusBarHeightPx());
+//        settings.setUserAgentString(webView.getSettings().getUserAgentString() + " StatusBarHeight/12312312321");
 
         // 解决alert不出现的问题
         webView.setWebChromeClient(new WebChromeClient());
@@ -76,7 +78,12 @@ public class MainActivity extends Activity {
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+
             window.setStatusBarColor(Color.TRANSPARENT);
+
+
+
         } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             //虚拟键盘也透明
@@ -170,5 +177,20 @@ public class MainActivity extends Activity {
         float px = result / density;
 
         return px;
+    }
+
+    /**
+     * 返回按钮
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
